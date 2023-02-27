ec8adeb25f8ea6cb182cdabbfc957a2f32c2c63a
Chambeo <105461463+Chambeo@users.noreply.github.com>
2023-02-26 22:30:49 -0300

    Develop (#5)
    * First commit
    
    * Add basic routing (#1)
    
    Co-authored-by: Meze <mezequielabogado@gmail.com>
    
    * Feature/add build basics (#4)
    
    * Add docker-compose, dockerfile and template sql files.
    
    * Modify create tables script
    
    ---------
    
    Co-authored-by: Meze <mezequielabogado@gmail.com>
    
    ---------
    
    Co-authored-by: Meze <mezequielabogado@gmail.com>

---
+++ sql/create_tables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE public.Users (
+                       id serial primary key,
+                       email varchar(250) not null,
+                       password varchar(250) not null,
+                       first_name varchar(250) not null,
+                       last_name varchar(250) not null,
+                       phone_code varchar(10),
+                       phone_number varchar(25),
+                       address_street varchar(250) not null,
+                       address_number varchar(10),
+                       address_details varchar(150),
+                       zip_code varchar(25),
+                       province varchar(200),
+                       city varchar(250),
+                       account_verified boolean default false,
+                       agree_terms_and_conditions boolean default false,
+                       profile_completed boolean default false,
+                       created_at timestamp without time zone NULL DEFAULT CURRENT_TIMESTAMP,
+                       updated_at timestamp without time zone NULL,
+                       deleted_at timestamp without time zone NULL
+);
+
+
+
+--ALTER TABLE user_lists ADD CONSTRAINT user_lists_list_id_fk FOREIGN KEY (list_id) REFERENCES lists(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+--ALTER TABLE user_lists ADD CONSTRAINT user_lists_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+
+--TODO cambiar las tablas por las correspondientes de la forma mas noSql posible
