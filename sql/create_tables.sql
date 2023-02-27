64bcde42b30b641171d9ffe18f28e458b63bc2ec
Meze <mezequielabogado@gmail.com>
2023-02-26 22:26:01 -0300

    Modify create tables script

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,59 +1,29 @@
-CREATE TABLE IF NOT EXISTS product (
-                                       product_id INT NOT NULL,
-                                       name varchar(250) NOT NULL,
-    PRIMARY KEY (product_id)
-    );
-
-
-CREATE TABLE IF NOT EXISTS users (
-                              id serial PRIMARY KEY,
-                              "name" text not null,
-                              email text NOT NULL UNIQUE,
-                              password text NOT NULL,
-                              role text NOT NULL,
-                              created_at timestamp without time zone NULL DEFAULT CURRENT_TIMESTAMP,
-                              updated_at timestamp without time zone NULL,
-                              deleted_at timestamp without time zone NULL
+CREATE TABLE public.Users (
+                       id serial primary key,
+                       email varchar(250) not null,
+                       password varchar(250) not null,
+                       first_name varchar(250) not null,
+                       last_name varchar(250) not null,
+                       phone_code varchar(10),
+                       phone_number varchar(25),
+                       address_street varchar(250) not null,
+                       address_number varchar(10),
+                       address_details varchar(150),
+                       zip_code varchar(25),
+                       province varchar(200),
+                       city varchar(250),
+                       account_verified boolean default false,
+                       agree_terms_and_conditions boolean default false,
+                       profile_completed boolean default false,
+                       created_at timestamp without time zone NULL DEFAULT CURRENT_TIMESTAMP,
+                       updated_at timestamp without time zone NULL,
+                       deleted_at timestamp without time zone NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS lists (
-                              id serial PRIMARY KEY,
-                              "name" varchar(150) NULL,
-                              description varchar(150) NULL,
-                              invite_code text NULL,
-                              user_creator_id bigint NOT NULL,
-                              created_at timestamp without time zone null DEFAULT CURRENT_TIMESTAMP,
-                              updated_at timestamp without time zone null DEFAULT NULL,
-                              deleted_at timestamp without time zone null DEFAULT NULL
-);
 
+--ALTER TABLE user_lists ADD CONSTRAINT user_lists_list_id_fk FOREIGN KEY (list_id) REFERENCES lists(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+--ALTER TABLE user_lists ADD CONSTRAINT user_lists_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-CREATE TABLE IF NOT EXISTS user_lists (
-                                   id serial PRIMARY KEY,
-                                   list_id serial NOT NULL,
-                                   user_id bigint NOT NULL,
-                                   created_at timestamp without time zone NULL DEFAULT CURRENT_TIMESTAMP,
-                                   updated_at timestamp without time zone NULL,
-                                   deleted_at timestamp without time zone NULL
-);
-
-ALTER TABLE user_lists ADD CONSTRAINT user_lists_list_id_fk FOREIGN KEY (list_id) REFERENCES lists(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE user_lists ADD CONSTRAINT user_lists_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
-CREATE TABLE list_items (
-                              id serial PRIMARY KEY,
-                              list_id bigint NOT NULL,
-                              user_id bigint not null,
-                              title varchar(150) NULL,
-                              description TEXT NULL,
-                              is_done boolean NULL DEFAULT false,
-                              created_at timestamp without time zone NULL DEFAULT CURRENT_TIMESTAMP,
-                              updated_at timestamp without time zone NULL,
-                              deleted_at timestamp without time zone NULL
-);
-
-ALTER TABLE list_item ADD CONSTRAINT item_creator_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE list_item ADD CONSTRAINT list_id_fk FOREIGN KEY (list_id) REFERENCES lists(id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --TODO cambiar las tablas por las correspondientes de la forma mas noSql posible
